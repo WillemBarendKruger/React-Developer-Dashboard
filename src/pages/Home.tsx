653fa2160574dadeee0b6f2ac91f6ae29123bb8c
WillemBarendKruger <willem.kruger11@gmail.com>
2025-06-03 21:48:47 +0200

    Feature - Added header elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,16 @@ export const Home = () => {
     return(
         <div>
             {/* Header with searchbar & navigation controls */}
+            <header>
+                <div>
+                    <input type="text" />
+                <button>search</button>
+                </div>
+                <nav>
+                    <button>back</button>
+                    <button>forward</button>
+                </nav>
+            </header>
 
             {/* User cards */}
             <UserCard />
