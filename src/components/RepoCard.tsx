b86c607cefb4d433c56f5d527bf7a21ec6bcf9ec
WillemBarendKruger <willem.kruger11@gmail.com>
2025-06-04 13:31:25 +0200

    Bug - fixed styling error

---
+++ src/components/RepoCard.tsx
@@ -0,0 +1,11 @@
+import "../utils/RepoCard.css"
+
+export const RepoCard = () => {
+    return(
+        <div className="repo-card">
+            <h2>Repository Name</h2>
+            <p>Description of the repository goes here.</p>
+            <button>View More</button>
+        </div>
+    )
+}
